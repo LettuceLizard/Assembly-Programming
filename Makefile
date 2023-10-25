e6400304c8034f057c479f49cdb33df894c6f4da
Elliot Marcusson <elliot.marcusson58@live.com>
2023-10-25 08:02:46 +0200

    edit

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 .PHONY: clean
 
+# Why does this generate an error??
+all:
+	cd lexyacc-code_lab3 && ./build && gcc -o calc3i calc3b.c y.tab.c lex.yy.c -lfl
+
 clean:
-	rm -f lexyacc-code_lab3/*.exe lexyacc-code_lab3/lex.yy.c lexyacc-code_lab3/*.o lexyacc-code_lab3/y.tab.c lexyacc-code_lab3/y.tab.h 
+	rm -f lexyacc-code_lab3/*.exe lexyacc-code_lab3/lex.yy.c lexyacc-code_lab3/*.o lexyacc-code_lab3/y.tab.c lexyacc-code_lab3/y.tab.h lexyacc-code_lab3/*.s
--- x86-64-driver.sh
+++ x86-64-driver.sh
@@ -7,18 +7,24 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+
 # Get the input file and the base name for the output files
 INPUT="$1"
 BASENAME=$(basename "$INPUT" .calc)
 
+cd ~/Programmering-i-UNIX-Labb-2/Assembly-Programming/lexyacc-code_lab3
 ./build
-./calc3b.c "$INPUT" > "$BASENAME.s"
+gcc -o calc3i calc3b.c y.tab.c lex.yy.c -lfl
+
+
+#echo "./calc3i < ../"$INPUT" "
+./calc3i < ../"$INPUT" #> "$BASENAME.s"
 
 # Assemble the generated assembly code to produce an object file
-gcc -c "$BASENAME.s" -o "$BASENAME.o"
+#gcc -c "$BASENAME.s" -o "$BASENAME.o"
 
 # Link the object file to produce the final executable
-gcc "$BASENAME.o" -o "$BASENAME"
+#gcc "$BASENAME.o" -o "$BASENAME"
 
 # Cleanup
-rm "$BASENAME.o"
+#rm "$BASENAME.o"
